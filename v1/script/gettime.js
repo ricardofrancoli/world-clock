5bc8f03294b0b08267638c162cf122026705d7f0
Ricardo <admin@admins-mbp.home>
2020-06-05 16:03:07 +0100

    search on input working now?

--- v1/script/gettime.js
+++ v1/script/gettime.js
@@ -11,17 +11,71 @@ async function getUnixTime() {
             localTime.textContent = `${h}:${m.substr(-2)}:${s.substr(-2)}`;
             unixtime += 1;
         };
+        timeCount();
         setInterval(timeCount, 1000);
     } catch(err) {
         localTime.textContent = `...ooops! There must've been an error. Try reloading the page :). Error: `, err;
     }
 };
 
-async function getTimeZone() {
-    const res = await axios.get('http://worldtimeapi.org/api/ip/');
-    const localPlace = document.querySelector('#local-place');
-    return localPlace.textContent = res.data.timezone;
+// async function getTimeZone() {
+//     const res = await axios.get('http://worldtimeapi.org/api/ip/');
+//     const localPlace = document.querySelector('#local-place');
+//     return localPlace.textContent = res.data.timezone;
+// }
+
+// const search = document.querySelector('form').addEventListener('click', event => {
+//     console.log(search)
+// })
+
+async function townSearch(search) {
+    const res = await axios.get('http://api.geonames.org/postalCodeLookupJSON?', {
+        params: {
+            placename: search,
+            maxRows: 10,
+            username: 'ricardofrancoli'
+        },
+    });
+
+    console.dir(res.data.postalcodes)
+    // console.dir(search)
+
+    // return res.data.postalcodes
+
+    // const towns = res.data.postalcodes;
+    // for (let town of towns) {
+    //     console.dir(town)
+    //     console.log(`lng: ${town.lng}, lat: ${town.lat}`);
+    // }
+
+    // const input = document.querySelector('input');
+    // const searchBtn = document.querySelector('button');
+
+    // const submit = searchBtn.addEventListener('click', event => {
+    //     event.preventDefault();
+    //     console.log(event)
+    //     console.log(input.value)
+    //     input.value = search;
+    // })
+    // console.log(input.value);
+    // console.log(submit);
 }
 
+const input = document.querySelector('input');
+
+let timeoutId;
+const onInput = event => {
+    if (timeoutId) {
+        clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+        townSearch(event.target.value);
+    }, 300)
+}
+
+input.addEventListener('input', onInput);
+
+
 getUnixTime();
-getTimeZone();
+// getTimeZone();
+// townSearch();
